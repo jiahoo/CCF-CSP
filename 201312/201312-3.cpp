d2c38379e241d9e5e2be99db0cc3f8480472a27b
jiahoo <inceptsun@gmail.com>
2020-03-19 14:36:10 +0800

    Update 201312-3.cpp
    加了个单调栈

--- 201312/201312-3.cpp
+++ 201312/201312-3.cpp
@@ -1,3 +1,8 @@
+/*
+脑抽写了个dp
+时间复杂度：O((2n)^2)
+空间复杂度：O(n^2)
+*/
 #include <bits/stdc++.h>
 #include <cstring>
 using namespace std;
@@ -35,3 +40,58 @@ int main(){
     Solution solution;
     cout<<solution.largestRectangleArea(heights)<<endl;
 }
+
+
+/*
+普通模拟
+时间复杂度：O(n^2)
+空间复杂度：O(1)
+*/
+
+#include <bits/stdc++.h>
+#include <cstring>
+using namespace std;
+const int INF = 0x3f3f3f3f;
+#define freopen(); freopen("in.txt","r",stdin);
+
+
+int main(){
+    freopen();
+    int n;cin>>n;
+    int ans = 0;
+    int heights[n];
+    for(int i=0;i<n;++i) cin>>heights[i];
+    for(int i=0;i<n;++i){
+        int minHeight = heights[i];
+        for(int j=i;j<n;++j){
+            minHeight = min(minHeight,heights[j]);
+            ans = max(ans,(j-i+1)*minHeight);
+        }
+    }
+    cout<<ans<<endl;
+}
+
+/*
+单调栈
+还没搞懂欸
+*/
+
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        int n = heights.size();
+        heights.push_back(0);
+        stack<int> s;
+        int area = 0;
+        for(int i=0;i<n+1;i++){
+            while(!s.empty()&&heights[s.top()]>heights[i]){
+                int h = heights[s.top()];
+                s.pop();
+                area = max(area,h*(s.empty()?i:(i-s.top()-1)));
+            }
+            s.push(i);
+        }
+        return area;
+    }
+};
+
